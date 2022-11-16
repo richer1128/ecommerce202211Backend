fe7bd5124fb43f156c8db05f0342526ee75c0a3a
richer.h <mr.mickeyeric@gmail.com>
2022-11-16 14:28:40 +0800

    git push

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    day: "1",
+    name: "A5WagYu",
+    category: "steak",
+    image: "/images/A5Wagyu.jpg",
+    price: 2500,
+    taste: "smooth",
+    city: "TaiPai",
+    numReviews: 10,
+    description: "This A5WagYu is divine ,I am just joking",
+  },
+  {
+    day: "2",
+    name: "AmericanSteakSet",
+    category: "combo",
+    image: "/images/AmericanSteakSet.jpg",
+    price: 1200,
+    taste: "tasty",
+    city: "TaiPai",
+    description:
+      "The AmericanSteakSet with fries of caramelized peaches is exquisite ,I am just joking",
+  },
+  {
+    day: "3",
+    name: "BritishDoubleDessert",
+    category: "combo",
+    image: "/images/BritishDoubleDessert.jpg",
+    price: 1000,
+    taste: "tasty",
+    city: "TaiPai",
+    description: "That's great for weekend afternoons",
+  },
+  {
+    day: "4",
+    name: "SaladAndCoke",
+    category: "SinglePoint",
+    image: "/images/SaladAndCoke.jpg",
+    price: 250,
+    taste: "juicy",
+    city: "TaiPai",
+    description: "Not bad ",
+  },
+  {
+    day: "5",
+    name: "TaiwaneseOyster",
+    category: "SinglePoint",
+    image: "/images/TaiwaneseOyster.jpg",
+    price: 250,
+    taste: "juicy",
+    city: "TaiPai",
+    description: "That's make me strong , ha ha",
+  },
+];
+
+export default products;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import products from "./products.js";
 
 const app = express();
+// const express = require("express");
+// const cors = require("cors");
+import cors from "cors";
 
+app.use(cors());
 app.get("/api/products", (req, res) => {
-  res.send(productsData.products);
+  res.send(products);
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-
+  console.log(`Server listening on port ${port}`);
 });
